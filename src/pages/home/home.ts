fd6cdae2cb1738873e821a405e0b7c3647aeee53
Bookie Oandasan <bookieoandasan@gmail.com>
2018-09-03 22:25:54 -0700

    Made scrum available

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,35 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private scrumers:string[];
 
   constructor(public navCtrl: NavController) {
 
+    this.scrumers = ['Bookie','Chris','Shawn', 'Kevin', 'Sofia','Lisa',
+                    'Paula', 'Chad','Paul', 'Ryan'] 
+  }
+  randomList(){
+    this.scrumers = this.shuffle(this.scrumers);
+
+  }
+
+  shuffle(array)  {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
   }
 
 }
